b46b9caf561bb7838ce10e5556ce6bf3c64a62fb
VihnevskiyAV <vishnevskiy1986@gmail.com>
2022-05-01 19:12:40 +0300

    +Gitognore

--- Task_5.10_5.11.php
+++ Task_5.10_5.11.php
@@ -1,6 +1,46 @@
 <?php
 /* Доработайте класс Point, добавив ко всем полям и функциям правильные типы данных.*/ 
+class Point {
+    private float $x;
+    private float $y;
+    private float $z;
+    private static int $counter = 0;
 
+    public function __construct(float $x = null, float $y = null) {
+        $this->x = $x;
+        $this->y = $y;
+        self::$counter++;
+    }
+    public function __toString() {
+        return "Точка с координатами ($this->x , $this->y): <br>";
+    }
+
+    public function __get($name) {
+       if ($name == 'x')  return $this->x;
+       if ($name == 'y')  return $this->y;
+       if ($name == 'z')  return "Класс Point работает только в двумерном пространстве <br>";
+       else return "Такого свойства нет! <br>";
+    }
+    public function __set($name, $value) {
+        if ($name == 'z') echo "Класс Point работает только в двумерном пространстве <br>";
+        return $this->$name = $value;
+    }
+
+    public function __call($metod, $arg){
+        echo "Метод $metod не существует<br>";
+    }
+    public function __sleep() {
+        return ['x', 'y'];
+    }
+
+    public static function getCounter() {
+        return self::$counter;
+    }
+    public function __clone() {
+        echo 'Клонирование<br />';
+    }
+
+}
 /* Создайте абстрактный класс Log с абстрактным методом add() и строковым параметром (некоторое сообщение).*/ 
 
 /* Добавьте конструктор в созданный класс, в котором в качестве параметра передаётся строка – путь к файлу лога.*/ 
